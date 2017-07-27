38657b3dc2ec3ecb7072f8ea51533b6ef52062c6
Durev <vincent.durewski@gmail.com>
2017-07-27 15:07:04 +0200

    finish pimp the page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def vince
+  end
+
+  def juan
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#home'
+  root to:'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get 'about/vince', to: 'static_pages#vince', as: 'test1'
+  get 'about/juan', to: 'static_pages#juan', as: 'juan'
   get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
